13775e51c662a4abe3ac5a241cdf1d0d77765cdb
despachantebm <despachantebm@gmail.com>
2026-01-26 14:21:55 -0300

    otimizaççao frontend e backend

--- sisgpo-frontend/dev-dist/sw.js
+++ sisgpo-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4qt15lp2pbc"
+    "revision": "0.rsg18hbk6i8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
